f9e950c39d8ef0d7a0c7dbb1181456af0579e8ee
lucassdolv <lucassdo2003@gmail.com>
2024-05-16 16:42:43 -0300

    att controller

---
+++ src/repository/accountsRepository.java
@@ -0,0 +1,93 @@
+package repository;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import models.Games;
+
+public class accountsRepository {
+	private List<Games> contas;
+	private File database;
+
+	public accountsRepository() {
+		this.database = new File("database-contas.txt");
+		this.contas = new ArrayList<>();
+	}
+	private void loadContas() {
+		try (Scanner sc = new Scanner(database)){
+			while (sc.hasNextLine()) {
+				String[] data = sc.nextLine().split(";");
+				if (data.length >= 7) {
+					Games conta = new Games();
+					conta.setId(Integer.parseInt(data[0]));
+					conta.setNickName(data[1]);
+					conta.setPassword(data[2]);
+					conta.setTeamName(data[3]);
+					conta.setCoins(Double.parseDouble(data[4]));
+					conta.setEAFCpoints(Double.parseDouble(data[5]));
+					conta.setExp(Double.parseDouble(data[6]));
+				}
+			}
+		}catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado, criando um novo!"); 
+		}
+	}
+	public void updateConta (Games updatedConta) {
+		for (int i = 0; i > contas.size(); i++) {
+			if (contas.get(i).getId() == updatedConta.getId()) {
+				contas.set(i, updatedConta);
+				saveContas();
+				break;
+			}
+		}
+	}
+	public Games getContaById(int id) {
+		for (Games conta : contas) {
+			if(conta.getId() == id) {
+				return conta;
+			}
+		}
+		return null;
+	}
+	
+	public List<Games> buscarTodos() {
+		return new ArrayList<>(contas);
+	}
+	public void deleteFly(int id) {
+		contas.removeIf(conta -> conta.getId() == id);
+		saveContas();
+	}
+	public void addConta(Games conta) {
+		conta.setId(1);;
+		contas.add(conta);
+		saveContas();
+	}
+	private void saveContas() {
+		try (PrintWriter writer = new PrintWriter(new FileOutputStream(database, false))){
+			for(Games conta : contas) {
+				String data = conta.getId() + ";" + conta.getNickName() + ";" + conta.getPassword() + ";" + conta.getTeamName() + ";" + conta.getCoins() + ";" + conta.getEAFCpoints() + ";" + conta.getExp();
+				writer.println(data);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO: handle exception
+			System.out.println("Arquivo database não encontrado!");
+		}
+	}
+	
+	
+	private int getNextId() {
+		int maxId = 0;
+		for (Games conta : contas) {
+			if (conta.getId() > maxId) {
+				maxId = conta.getId();			}
+		}
+		return maxId + 1;
+	}
+	
+}
+
